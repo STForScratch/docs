36a87d809239d9654499500adc6f6c1abefab7ad
rgantzos <info@gantzos.com>
2022-11-13 14:57:45 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Home`}
+      description="The official documentation for ScratchTools, a free and open-source browser extension for Scratch.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
