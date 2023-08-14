0b927d30d0ac479cdfe49e0383e90aad894fd3ec
rgantzos <86856959+rgantzos@users.noreply.github.com>
2023-08-13 22:24:23 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,7 +120,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ScratchTools`,
       },
       prism: {
         theme: lightCodeTheme,
